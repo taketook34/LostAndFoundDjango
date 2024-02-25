1ecec19eebc3c355c520dc0272ae4d265d3e9924
taketook34 <taketook34@gmail.com>
2024-02-25 11:06:36 +0200

    Changing view functions on CBV

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('postlist/', postlist, name='postlist'),
+    path('postlist/', PostList.as_view(), name='postlist'),
     path('contacts/', contacts, name="contacts"),
-    path('postlist/<int:post_id>/', show_post, name="post"),
+    path('postlist/<int:post_id>/', PostShow.as_view(), name="post"),
     path('registration/', register_page, name="register"),
     path('login/', login_page, name='login')
 ]
--- app/views.py
+++ app/views.py
@@ -1,23 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def home(request):
-    
-    return render(request, 'app/homepage.html')
+
+    return render(request, 'app/homepage.html', {'page_title': 'Головна сторінка'})
     
 #assets/
 
-def postlist(request):
-    context = {
-        'posts': Post.objects.all(),
-        # 'posts': [
-        #     {"title": "Заголовок 1", "description": "Опис 1", "number": 1},
-        #     {"title": "Заголовок 2", "description": "Опис 2", "number": 2},
-        # ]
-    }
+# def postlist(request):
+#     context = {
+#         'posts': Post.objects.all(),
+#         'page_title': 'Знахідки'
+#     }
 
-    return render(request, 'app/postlist.html', context=context)
+#     return render(request, 'app/postlist.html', context=context)
+
+class PostList(ListView):
+    model = Post
+    template_name = "app/postlist.html"
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Знахідки'
+        return context
+
+    def get_queryset(self):
+        return Post.objects.all()
     
 
 def contacts(request):
@@ -26,25 +37,40 @@ def contacts(request):
             {"name":"Хусаінов Дмитро ІО-11"},
             {"name":"Шинкарчук Богдан ІО-11"},
             {"name":"Столярчук Микола ІО-11"}
-        ] 
+        ],
+        'page_title': 'Про нас' 
 
     }
 
     return render(request, 'app/contacts.html', context=context)
     
 
-def show_post(request, post_id):
-    """ ЕНдпоінт для кожної сторінки"""
+# def show_post(request, post_id):
+#     """ ЕНдпоінт для кожної сторінки"""
+#     post = get_object_or_404(Post, id=post_id)
 
-    return render(request, "app/showpost.html", context={'number': post_id})
+#     return render(request, "app/showpost.html", context={'post': post, 'page_title': f'Пост \"{post.name}\"'})
+
+class PostShow(DetailView):
+    model = Post
+    template_name = 'app/showpost.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = f'Пост'
+        return context
+
+    def get_object(self):
+        return Post.objects.get(pk=self.kwargs['post_id'])
 
 
 def register_page(request):
 
-    return render(request, "app/registerpage.html")
+    return render(request, "app/registerpage.html", {'page_title': 'Реєстрація'})
 
 def login_page(request):
 
-    return render(request, "app/loginpage.html")
+    return render(request, "app/loginpage.html", {'page_title': 'Логін'})
 
 
